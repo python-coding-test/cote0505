d51f7fc28a5768c15772fe007c1d4587b8e493b3
kiiiiv <lizzy2@naver.com>
2024-05-19 03:54:56 +0900

    .

--- 2주차/2-2번문제/윤예원.py
+++ 2주차/2-2번문제/윤예원.py
@@ -1,20 +1,23 @@
 import math, sys
 input = sys.stdin.readline
 a,b = map(int, input().split())
-arr = [False]+[False]+[True for _ in range(2,b+1)]
+
+def countnum(n): #소수가 아닌 것의 개수
+    if  n<= 1:
+        return 0
+    else:
+        arr_val = set()
+        for i in range(2, int(math.sqrt(n))+1):        
+            j = 2
+            while i*j <= n:
+                arr_val.add(i*j)
+                j+=1
+        return len(arr_val)
 
 
-for i in range(2, int(math.sqrt(b))+1):
-    if arr[i] == True:
-        j = 2
-        while i*j <= b:
-            arr[i*j] = False
-            j+=1
+count_val = b - (countnum(b) - countnum(a)) -2
 
-
-count = sum(arr[a:b+1])
-
-print(count)
+print(count_val)
 
 
 
