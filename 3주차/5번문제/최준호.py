c0050d606115258cbdfcd178c020bd317a90dd80
Junho Choi <tig9659@ajou.ac.kr>
2024-05-25 11:23:04 +0900

    Add : 1753

---
+++ 3주차/5번문제/최준호.py
@@ -0,0 +1,28 @@
+import heapq
+from collections import defaultdict
+
+V, E = map(int, input().split())
+K = int(input())
+
+edge = defaultdict(list)
+for _ in range(E):
+    u, v, w = map(int, input().split())
+    edge[u].append((v, w))
+
+res = [float('inf')] * (V + 1)
+res[K] = 0
+q = [(0, K)]
+while q:
+    d, v = heapq.heappop(q)
+
+    for nv, w in edge[v]:
+        nd = d + w
+        if nd < res[nv]:
+            res[nv] = nd
+            heapq.heappush(q, (nd, nv))
+
+for i in range(1, V + 1):
+    print(res[i] if res[i] != float('inf') else 'INF')
+
+# pypy만 통과됩니다
+
